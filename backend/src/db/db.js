ddfc68706ab7809b45443f632dc204cd46d9ea96
ainobin <ainobin9@gmial.com>
2025-04-20 22:56:15 +0600

    transaction test 1

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -5,9 +5,10 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         console.log('Connecting to MongoDB...');
-        console.log(`url: ${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`url: ${process.env.MONGO_URI} /// dbName: ${DB_NAME}`);
         
-        const connectionInstanace = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstanace = await mongoose.connect(process.env.MONGO_URI, { dbName: DB_NAME });
+
         console.log(`MongoDB Connected: ${connectionInstanace.connection.host}`);
     
     } catch (error) {
